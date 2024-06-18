674681a88ca542e0fbc6eaf120db180df813525f
tzadm <94993624+tzadm@users.noreply.github.com>
2024-06-18 11:41:19 +0300

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -12,12 +12,11 @@ class House:
             print('Такого этажа не существует')
 
         else:
-            for i in range( 1, new_floor +1):
+            for i in range(1, new_floor + 1):
                 print(i)
 
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
-h2.go_to(12)
-
+h2.go_to(10)
